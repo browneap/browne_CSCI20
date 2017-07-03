9b4b7cd5bc3de5c90d57767cd7a3cca585d5f599
browneap <browneap@butte.edu>
2017-07-03 20:28:21 +0000

    Update 1.1

---
+++ topic1/lecture1.5.cpp
@@ -0,0 +1,21 @@
+/* Created By: April Browne
+ * Created On: 7/3/2017
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    const PI = 3.14;
+    unsigned int x = 100000000000;
+    char alpha = 'a';
+    string myString = "Hello World";
+    
+    cout<<PI*x<<endl;
+    cout<<myString<<" "<<alpha<<endl;
+}
+
+//Identify with a //* 2-3 things you think are important
+//Identify with a //? 2-3 things that are incorrect, unclear, or you have questions about
