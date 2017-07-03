5b35f5fb16476b42068c167fd18bc3f66c55f011
browneap <browneap@butte.edu>
2017-07-03 20:45:49 +0000

    Update 1.1

--- topic1/lecture1.6.cpp
+++ topic1/lecture1.6.cpp
@@ -4,7 +4,27 @@
  #include <iostream>
  using namespace std;
  
+ struct monster{
+  string head;
+  string eyes;
+  string ears;
+  string mouth;
+  string nose;
+ };
+ 
  int main()
  {
+   monster monsterA;
+   monsterA.head = "Zombus";
+   monsterA.eyes = "Vegitas";
+   monsterA.ears = "Wackus";
+   monsterA.nose = "Vegitas";
+   monsterA.mouth = "Wackus";
+   
+   cout<<"Head:"<<monsterA.head<<endl;
+   cout<<"Eyes:"<<monsterA.eyes<<endl;
+   cout<<"Ears:"<<monsterA.ears<<endl;
+   cout<<"Nose:"<<monsterA.nose<<endl;
+   cout<<"Mouth:"<<monsterA.mouth<<endl;
      
  }
